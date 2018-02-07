14beebbe686145d3c7036dbc5fc8702a5b194b53
Rodrigo Oliveira <rodrigooliveira@rodrigo.local>
2018-02-07 08:43:44 -0200

    salvando edição, remoção e novos cartões no local storage

--- js/mural/Mural.js
+++ js/mural/Mural.js
@@ -1,20 +1,35 @@
 const Mural = (function(_render, Filtro){
     "use strict"
-    let cartoes = []
+    let cartoes = JSON.parse(localStorage.getItem("cartoes") || []).map(cartaoLocal => new Cartao(cartaoLocal.conteudo, cartaoLocal.tipo));
+    cartoes.forEach(cartao => {
+        preparaCartao(cartao)
+    });
     const render = () => _render({cartoes: cartoes, filtro: Filtro.tagsETexto});
+    render();
 
     Filtro.on("filtrado", render)
+
+    function salvaCartoes() {
+        localStorage.setItem("cartoes", JSON.stringify(
+            cartoes.map(cartao => ({conteudo: cartao.conteudo, tipo: cartao.tipo}))
+        ));
+    }
+
+    function preparaCartao(cartao) {
+        cartao.on("mudanca.**", salvaCartoes)
+        cartao.on("remocao", () => {
+            cartoes = cartoes.slice(0)
+            cartoes.splice(cartoes.indexOf(cartao), 1)
+            salvaCartoes();
+            render()
+        })
+    }
 
     function adiciona(cartao){
         if(logado){
             cartoes.push(cartao)
-            // salvaCartoes()
-            cartao.on("mudanca.**", render)
-            cartao.on("remocao", ()=>{
-                cartoes = cartoes.slice(0)
-                cartoes.splice(cartoes.indexOf(cartao),1)
-                render()
-            })
+            salvaCartoes();
+            preparaCartao(cartao);
             render()
             return true
         } else {
